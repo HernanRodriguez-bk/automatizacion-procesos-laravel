c173d0e852df1ac6b2df1801ccd94d8cdd0530aa
Hernan Rodriguez <hernanrodriguez.bk@gmail.com>
2026-02-12 19:36:22 -0300

    Preparacion para deploy en Render

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Iluminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Gate;
 
 class AppServiceProvider extends ServiceProvider
 {
